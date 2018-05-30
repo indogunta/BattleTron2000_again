617cd765f9da0695d16cb928b7f486d6d57c4c77
Michael Kaplor <mkaplor@gmail.com>
2018-05-30 10:32:56 -0700

    premerge

--- Assets/Michael/_scrripts/RotateHead.cs
+++ Assets/Michael/_scrripts/RotateHead.cs
@@ -19,7 +19,7 @@ public class RotateHead : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime, Space.World);
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ));
     }
 
 
