3d0c904eaadbadd2c36e2ee8cc9d22f13582a4db
Michael Kaplor <mkaplor@gmail.com>
2018-06-04 17:30:39 -0700

    nedd kill

---
+++ Assets/Michael/_scrripts/ObjectiveHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveHandler : MonoBehaviour 
+
+{
+	
+	public Objectives currentObjective;
+
+	void Start()
+	{
+		List<Objectives> obbys = new List<Objectives> ();
+		currentObjective.isAchieved = false;
+	}
+
+	void CheckObjectives()
+	{
+		
+	}
+
+
+}
---
+++ Assets/Michael/_scrripts/Objectives.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives  
+{
+
+	public string name;
+	public string mesage;
+
+
+	public GameObject[] condition;
+
+	public bool isAchieved;
+
+
+
+}
