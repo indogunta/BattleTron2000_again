2c3266b54e4d57d89a55fc062239bad779a4c07d
Michael Kaplor <mkaplor@gmail.com>
2018-06-20 09:47:16 -0700

    prebeta

---
+++ Assets/Michael/_scrripts/Turret.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float turningRate;
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private GameObject barrelTip;
+
+    private Vector3 barrelForward;
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        { 
+                // .. if it's enemy, look at it and shoot it
+                transform.LookAt(other.transform);
+			barrelForward = barrelTip.transform.forward;
+                StartCoroutine("FireDelay");
+                FireTurret();
+                StartCoroutine("FireDelay");
+        }
+        
+    }
+
+    void FireTurret()
+    {
+        // Instantiate the bullets as gameObjects
+        var bullet = Instantiate(projectile, barrelTip.transform.position, Quaternion.identity) as GameObject;
+        // Add the impulse force to make the bullets move
+        bullet.GetComponent<Rigidbody>().AddForce(barrelForward * 1000f, ForceMode.Impulse);
+    }
+
+    // So that each bullet has a delay between it being fired
+    IEnumerator FireDelay()
+    {
+        yield return new WaitForSeconds(speed);
+    }
+}
