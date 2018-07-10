35db9fb34b42069ed4aa7ef6fd5a62e4d6a3b87a
Carelle <steamtik@gmail.com>
2018-07-09 17:22:49 -0700

    Fixed UI and got the loading to work right

---
+++ Assets/Kim/Scripts/MouseLock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+	
+}
